d17dbb56d5fc66e489081dcf80a85da7accee19e
SALONBLOOM54 <klok-nikita@mail.ru>
2021-04-01 00:10:53 +0300

    Calculator

--- main_test.go
+++ main_test.go
@@ -1,11 +1,11 @@
-package main
+// package main
 
-import "testing"
+// import "testing"
 
-func TestSayHi(t *testing.T) {
-    expected := "Hi Marco"
-    greeting := sayHi("Marco")
-    if greeting != expected {
-        t.Errorf("Greeting was incorrect, got: '%s', want: '%s'", greeting, expected)
-    }
-}
+// func TestSayHi(t *testing.T) {
+//     expected := "Hi Marco"
+//     greeting := sayHi("Marco")
+//     if greeting != expected {
+//         t.Errorf("Greeting was incorrect, got: '%s', want: '%s'", greeting, expected)
+//     }
+// }
